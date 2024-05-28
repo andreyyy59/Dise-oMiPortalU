d5e280c5bd5052e57273ec996adef763841b385d
Andrey David Morales Diaz <165956683+andreyyy59@users.noreply.github.com>
2024-05-28 15:55:44 -0500

    Delete MIPORTALU proyecto final directory

--- MIPORTALU proyecto final/Codigo/resetpassword.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("resetPasswordForm").addEventListener("submit", function(event) {
-    event.preventDefault(); // Evitar el envío del formulario por defecto
-
-    var email = document.getElementById("email").value;
-
-    alert("Se ha enviado un correo electrónico con instrucciones para restablecer la contraseña a: " + email);
-});
